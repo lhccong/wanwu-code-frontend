d6d6c2f97e61f11f755f2df3067d7378c59c08a4
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2023-09-28 09:36:36 +0800

    💥 feat(compiler): update '测试action' option

--- src/pages/wanwu/Bi/AddChart/index.tsx
+++ src/pages/wanwu/Bi/AddChart/index.tsx
@@ -121,7 +121,7 @@ const AddChart: React.FC = () => {
                     loading={submitting}
                     disabled={submitting}
                   >
-                    提交66
+                    提交🌈
                   </Button>
                   <Button htmlType="reset">重置</Button>
                 </Space>
